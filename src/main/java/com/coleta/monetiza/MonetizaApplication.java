2bab77a70170e22b376e15315992d915a3ba8642
audrinp <audrincp@hotmail.com>
2024-09-26 12:09:25 -0300

    init

---
+++ src/main/java/com/coleta/monetiza/MonetizaApplication.java
@@ -0,0 +1,13 @@
+package com.coleta.monetiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonetizaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coleta/monetiza/MonetizaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coleta.monetiza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonetizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
